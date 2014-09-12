35b0e3e3553e018d4bb3cf0af91fcc9bbff0a7f3
mpalacio <mikelito92@gmail.com>
2014-09-12 14:40:21 +0800

    FOR MASTER: initialize repository

---
+++ config/Tables/transactions.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2014 at 08:24 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `aov-canteen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `stock_id` int(20) NOT NULL,
+  `count` int(20) NOT NULL,
+  `customer_id` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`,`type`,`stock_id`,`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
